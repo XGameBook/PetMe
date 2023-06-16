ece6cb4fc61478dc53118d6476fcf703fb0e83bd
XGameBook <136720357+XGameBook@users.noreply.github.com>
2023-06-16 12:43:47 +0300

    Update Osnova.cs

--- Pet me/Assets/Osnova.cs
+++ Pet me/Assets/Osnova.cs
@@ -10,7 +10,7 @@ public class Osnova : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Head= 0;// head 
+        Head= 0;// head ffff
     }
 
     // Update is called once per frame
