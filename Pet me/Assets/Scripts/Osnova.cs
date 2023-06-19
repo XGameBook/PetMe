4ed5bcaf80443c966c07272ec2e19afb1a596883
XGameBook <136720357+XGameBook@users.noreply.github.com>
2023-06-19 14:48:37 +0300

    2
    2

---
+++ Pet me/Assets/Scripts/Osnova.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Osnova : MonoBehaviour
+{   public float Head= 1;
+    public float Time1;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Head= 0;// head ffff
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time1 += Time.deltaTime;
+    }
+}
